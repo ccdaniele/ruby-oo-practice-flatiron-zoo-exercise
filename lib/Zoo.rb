0f0982773bfca6638021f27c61992147e2634829
Daniel <danieleduardocald@gmail.com>
2020-12-18 08:24:45 -0500

    commit tree 3606d8a4e9156c73de133accae1afddb19a77932

--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,16 @@
 class Zoo
 
+    attr_accessor :name, :location
+
+    @@all = []
+
+    def initialize(name, location)
+        @name = name 
+        @location = location
+        @@all << self
+    end
+
+    
+
+
 end
